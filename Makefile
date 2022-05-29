4aa3df379076a276fdaff4b0d50a52a9ae504080
alex123012 <makhonin.a.ru@gmail.com>
2022-05-29 12:17:14 +0300

    Refactor repository structure

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: build install clean
+
+build:
+	CGO_ENABLED=0 go build -o bin/annotations-exporter github.com/alex123012/annotations-exporter/cmd/annotations-exporter
+install:
+	CGO_ENABLED=0 go install github.com/alex123012/annotations-exporter/cmd/annotations-exporter
+clean:
+	rm -f $$GOPATH/bin/annotations-exporter
+	rm -f bin/*
--- cmd/annotations-exporter/main.go
+++ cmd/annotations-exporter/main.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/pprof"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"runtime"
 	"syscall"
-	"time"
 
-	ar "github.com/alex123012/annotations-exporter/src/apiresources"
-	rc "github.com/alex123012/annotations-exporter/src/resourcecontroller"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	ar "github.com/alex123012/annotations-exporter/pkg/apiresources"
+	rc "github.com/alex123012/annotations-exporter/pkg/resourcecontroller"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 	v1 "k8s.io/api/core/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 )
 
@@ -98,71 +88,4 @@ func main() {
 	if err := cmd.ExecuteContext(ctx); err != nil {
 		klog.Exitln(err)
 	}
-}
-
-func GenerateNewConfig(local bool) *rest.Config {
-	var clusterConfig *rest.Config
-	var err error
-
-	switch local {
-	case true:
-		clusterConfig, err = clientcmd.BuildConfigFromFlags("", filepath.Join(os.Getenv("HOME"), ".kube", "config"))
-	case false:
-		clusterConfig, err = rest.InClusterConfig()
-	}
-	if err != nil {
-		klog.Exitln(err)
-	}
-
-	return clusterConfig
-}
-
-func RunMetricsServer(ctx context.Context, controller *rc.ResourceController, port int) error {
-	mux := http.NewServeMux()
-	svr := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: mux}
-
-	mux.Handle("/metrics", promhttp.Handler())
-	mux.HandleFunc("/__/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/__/pprof/trace", pprof.Trace)
-	mux.HandleFunc("/__/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/__/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		switch controller.CheckCacheSync() {
-		case true:
-			w.WriteHeader(http.StatusOK)
-		case false:
-			w.WriteHeader(http.StatusPreconditionFailed)
-		}
-	})
-
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		defer svr.Shutdown(ctx)
-		<-c
-	}()
-	return svr.ListenAndServe()
-}
-func SystemStats(ctx context.Context) error {
-	mem := &runtime.MemStats{}
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	go func() {
-		for {
-			cpu := runtime.NumCPU()
-			log.Println("CPU:", cpu)
-
-			rot := runtime.NumGoroutine()
-			log.Println("Goroutine:", rot)
-
-			// Byte
-			runtime.ReadMemStats(mem)
-			log.Println("Memory:", mem.Alloc/1024)
-
-			time.Sleep(2 * time.Second)
-			log.Println("-------")
-		}
-	}()
-	<-ctx.Done()
-	return nil
 }
